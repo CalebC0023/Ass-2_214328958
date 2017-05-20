487cc5f81e39f1c0bcb22a63c43ffe2e5e97fa46
CalebC0023 <user126206@103-246-99-70.moboware.com>
2017-05-20 23:12:19 +1000

    Second version

---
+++ SIT206_Ass2_214328958/SIT206_Ass2_214328958/EasyGameViewController.swift
@@ -0,0 +1,24 @@
+//
+//  EasyGameViewController.swift
+//  SIT206_Ass2_214328958
+//
+//  Created by user126206 on 5/20/17.
+//  Copyright © 2017 Deakin. All rights reserved.
+//
+
+import UIKit
+
+class EasyGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
